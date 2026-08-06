b5a2c249f2a547c50e91ff944b1c573c82cf3970
OkeLDF <otaviofolafor43@gmail.com>
2026-08-06 10:40:57 -0300

    robô completo

--- robot/src/main/java/com/uepg/robot/Robot.java
+++ robot/src/main/java/com/uepg/robot/Robot.java
@@ -14,6 +14,11 @@ import java.awt.geom.*;
 public class Robot extends Frame {
 
     // Canvas
+    private final Color robotBodyPrimaryColor = new Color(232, 232, 232);
+    private final Color robotBodySecondaryColor = Color.GRAY;
+    private final Color robotDetailColor = new Color(40,80, 255);
+    private final Color robotLightsColor = new Color(255, 80, 80);
+
     private final int canvasW = 480;
     private final int canvasH = 480;
     private final int canvasCenterX = canvasW / 2;
@@ -38,12 +43,12 @@ public class Robot extends Frame {
 
         Area robot = new Area();
 
-        robot.add(drawBody(g2d));
-        robot.add(drawHead(g2d));
         robot.add(drawLeftLeg(g2d));
         robot.add(drawRightLeg(g2d));
         robot.add(drawLeftArm(g2d));
         robot.add(drawRightArm(g2d));
+        robot.add(drawBody(g2d));
+        robot.add(drawHead(g2d));
 
         // Outline do robô inteiro
         g2d.setPaint(Color.BLACK);
@@ -58,8 +63,33 @@ public class Robot extends Frame {
 
         Area body = new Area(bodyRect);
 
-        g2d.setPaint(Color.GRAY);
+        g2d.setPaint(robotBodyPrimaryColor);
         g2d.fill(body);
+
+        AffineTransform backup = g2d.getTransform();
+        Rectangle2D.Double detail1Rect = new Rectangle2D.Double(
+            bodyX + 50, bodyY - 40, 30, bodyH * 2
+        );
+        AffineTransform rotacao = new AffineTransform();
+        rotacao.rotate(Math.toRadians(30), detail1Rect.getCenterX(), detail1Rect.getCenterY());
+        Area detail1 = new Area(detail1Rect);
+        detail1.transform(rotacao);
+        Area bodyArea = new Area(bodyRect);
+        detail1.intersect(bodyArea);
+        g2d.setPaint(robotDetailColor);
+        g2d.fill(detail1);
+
+        Rectangle2D.Double detail2Rect = new Rectangle2D.Double(
+            bodyX + 30, bodyY-40, 10, bodyH * 2
+        );
+        AffineTransform rotacao2 = new AffineTransform();
+        rotacao2.rotate(Math.toRadians(30), detail2Rect.getCenterX(), detail2Rect.getCenterY());
+        Area detail2 = new Area(detail2Rect);
+        detail2.transform(rotacao2);
+        detail2.intersect(bodyArea);
+        g2d.setPaint(robotDetailColor);
+        g2d.fill(detail2);
+        g2d.setTransform(backup);
 
         // Botões
         int btnD = 10;
@@ -68,7 +98,7 @@ public class Robot extends Frame {
         Ellipse2D.Double btn1 = new Ellipse2D.Double(
             btn1X, btnY, btnD, btnD);
 
-        g2d.setPaint(Color.RED);
+        g2d.setPaint(robotLightsColor);
         g2d.fill(btn1);
         g2d.setPaint(Color.BLACK);
         g2d.setStroke(new BasicStroke(3));
@@ -79,13 +109,11 @@ public class Robot extends Frame {
         Ellipse2D.Double btn2 = new Ellipse2D.Double(
             btn2X, btnY, btnD, btnD);
 
-        g2d.setPaint(Color.RED);
+        g2d.setPaint(robotLightsColor);
         g2d.fill(btn2);
         g2d.setPaint(Color.BLACK);
         g2d.setStroke(new BasicStroke(3));
         g2d.draw(btn2);
-
-
 
         body.add(new Area(btn1));
         body.add(new Area(btn2));
@@ -116,7 +144,7 @@ public class Robot extends Frame {
         head.add(new Area(headRect));
 
         // Cabeça
-        g2d.setPaint(Color.GRAY);
+        g2d.setPaint(robotBodyPrimaryColor);
         g2d.fill(head);
 
         // Olho
@@ -126,7 +154,7 @@ public class Robot extends Frame {
         Ellipse2D.Double eye = new Ellipse2D.Double(
             eyeX, eyeY, eyeCircleR, eyeCircleR);
 
-        g2d.setPaint(Color.RED);
+        g2d.setPaint(robotLightsColor);
         g2d.fill(eye);
         g2d.setPaint(Color.BLACK);
         g2d.setStroke(new BasicStroke(3));
@@ -190,29 +218,159 @@ public class Robot extends Frame {
     }
 
     private Area drawLeftArm(Graphics2D g2d) {
-        Area arm = new Area();
+        int outerR = 150;
+        Ellipse2D.Double outerArmCircle = new Ellipse2D.Double(
+            bodyX - (outerR / 2), bodyY + 10, outerR, outerR
+        );
+        
+        int innerR = outerR - 50;
+        Ellipse2D.Double innerArmCircle = new Ellipse2D.Double(
+            outerArmCircle.getCenterX() - (innerR / 2), outerArmCircle.getCenterY() - (innerR / 2), innerR, innerR
+        );
 
+        Rectangle2D.Double subRect = new Rectangle2D.Double(
+            0, bodyY + bodyH - 5, canvasCenterX, canvasCenterY
+        );
+        
+        Area arm = new Area(outerArmCircle);
+        arm.subtract(new Area(innerArmCircle));
+        arm.subtract(new Area(subRect));
+        g2d.setPaint(robotBodySecondaryColor);
+        g2d.fill(arm);
+        
+        int outerHandR = 60;
+        double handX = arm.getBounds().x - 16.5;
+        int handY = arm.getBounds().y + arm.getBounds().height - 5;
+        Ellipse2D.Double outerHandCircle = new Ellipse2D.Double(
+            handX, handY, outerHandR, outerHandR
+        );
+        
+        int innerHandR = outerHandR - 30;
+        Ellipse2D.Double innerHandCircle = new Ellipse2D.Double(
+            outerHandCircle.getCenterX() - (innerHandR / 2), outerHandCircle.getCenterY() - (innerHandR / 2), innerHandR, innerHandR
+        );
+
+        int subHandW = 15;
+        Rectangle2D.Double subHandRect = new Rectangle2D.Double(
+            innerHandCircle.getCenterX() - (subHandW / 2), innerHandCircle.getCenterY(), subHandW, 40
+        );
+
+        Area hand = new Area(outerHandCircle);
+        hand.subtract(new Area(innerHandCircle));
+        hand.subtract(new Area(subHandRect));
+        g2d.setPaint(robotBodyPrimaryColor);
+        g2d.fill(hand);
+
+        arm.add(hand);
 
         return arm;
     }
 
     private Area drawRightArm(Graphics2D g2d) {
-        Area arm = new Area();
+        int outerR = 150;
+        Ellipse2D.Double outerArmCircle = new Ellipse2D.Double(
+            bodyX + bodyW - (outerR / 2), bodyY + 10, outerR, outerR
+        );
+        
+        int innerR = outerR - 50;
+        Ellipse2D.Double innerArmCircle = new Ellipse2D.Double(
+            outerArmCircle.getCenterX() - (innerR / 2), outerArmCircle.getCenterY() - (innerR / 2), innerR, innerR
+        );
 
+        Rectangle2D.Double subRect = new Rectangle2D.Double(
+            canvasCenterX, bodyY + bodyH - 5, canvasCenterX, canvasCenterY
+        );
+        
+        Area arm = new Area(outerArmCircle);
+        arm.subtract(new Area(innerArmCircle));
+        arm.subtract(new Area(subRect));
+        g2d.setPaint(robotBodySecondaryColor);
+        g2d.fill(arm);
+        
+        int outerHandR = 60;
+        double handX = arm.getBounds().x + (arm.getBounds().width / 2) + 31.5;
+        int handY = arm.getBounds().y + arm.getBounds().height - 5;
+        Ellipse2D.Double outerHandCircle = new Ellipse2D.Double(
+            handX, handY, outerHandR, outerHandR
+        );
+        
+        int innerHandR = outerHandR - 30;
+        Ellipse2D.Double innerHandCircle = new Ellipse2D.Double(
+            outerHandCircle.getCenterX() - (innerHandR / 2), outerHandCircle.getCenterY() - (innerHandR / 2), innerHandR, innerHandR
+        );
+
+        int subHandW = 15;
+        Rectangle2D.Double subHandRect = new Rectangle2D.Double(
+            innerHandCircle.getCenterX() - (subHandW / 2), innerHandCircle.getCenterY(), subHandW, 40
+        );
+
+        Area hand = new Area(outerHandCircle);
+        hand.subtract(new Area(innerHandCircle));
+        hand.subtract(new Area(subHandRect));
+        g2d.setPaint(robotBodyPrimaryColor);
+        g2d.fill(hand);
+
+        arm.add(hand);
 
         return arm;
     }
 
     private Area drawLeftLeg(Graphics2D g2d) {
-        Area leg = new Area();
+        int legGap = 20;
+        int legH = 80;
+        int legW = 30;
+        Rectangle2D.Double legRect = new Rectangle2D.Double(
+            bodyX + (bodyW / 2) + legGap, bodyY + bodyH, legW, legH
+        );
+        g2d.setPaint(robotBodySecondaryColor);
+        g2d.fill(legRect);
 
+        Area leg = new Area(legRect);
+
+        int[] xPoints = {
+            leg.getBounds().x, leg.getBounds().x + legW, leg.getBounds().x + legW + 20, leg.getBounds().x
+        };
+        int[] yPoints = {
+            leg.getBounds().y + legH, leg.getBounds().y + legH, leg.getBounds().y + legH + 20, leg.getBounds().y + legH + 20
+        };
+        int nPoints = 4;
+
+        Polygon foot = new Polygon(xPoints, yPoints, nPoints);
+
+        g2d.setPaint(robotBodyPrimaryColor);
+        g2d.fill(foot);
+
+        leg.add(new Area(foot));
 
         return leg;
     }
 
     private Area drawRightLeg(Graphics2D g2d) {
-        Area leg = new Area();
+        int legGap = 20;
+        int legH = 80;
+        int legW = 30;
+        Rectangle2D.Double legRect = new Rectangle2D.Double(
+            bodyX + (bodyW / 2) - (legGap + legW), bodyY + bodyH, legW, legH
+        );
+        g2d.setPaint(robotBodySecondaryColor);
+        g2d.fill(legRect);
 
+        Area leg = new Area(legRect);
+
+        int[] xPoints = {
+            leg.getBounds().x, leg.getBounds().x - 20, leg.getBounds().x + legW, leg.getBounds().x + legW
+        };
+        int[] yPoints = {
+            leg.getBounds().y + legH, leg.getBounds().y + legH + 20, leg.getBounds().y + legH + 20, leg.getBounds().y + legH
+        };
+        int nPoints = 4;
+
+        Polygon foot = new Polygon(xPoints, yPoints, nPoints);
+
+        g2d.setPaint(robotBodyPrimaryColor);
+        g2d.fill(foot);
+
+        leg.add(new Area(foot));
 
         return leg;
     }
